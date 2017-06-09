f70be0707be4d67714ffe8f89888ee020a0b9714
eunice333 <eunicestar1@gmail.com>
2017-06-09 21:26:58 +0800

    Add files via upload

---
+++ loadDllCsharp/loadDllCsharp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rol = HRobot.Connect("192.168.0.3");
+            double[] point = { 0, 0, 0, 0, -90, 0 };
+            Console.ReadLine();
+            
+
+        }
+    }
+}
